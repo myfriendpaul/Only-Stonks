ff204c60f568c21e53b8e64325585e042e1de85e
Paul Gonzalez <paulgonzalez@Pauls-MacBook-Pro.local>
2021-08-03 12:04:18 -0700

    HTML and CSS partially completed. Working on JS functions

---
+++ OnlyStonks/navscript.js
@@ -0,0 +1,22 @@
+// Nav animation
+const navSlide = () => {
+  const burger = document.querySelector('.burger')
+  const nav = document.querySelector('.nav-links')
+  const navLinks = document.querySelectorAll('.nav-links li')
+  // Toggle Nav
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active')
+
+    // Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = ''
+      } else {
+        link.style.animation = link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .5}s`
+      }
+    })
+  })
+}
+
+navSlide();
+
