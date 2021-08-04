0a6009a4e17874c9fda70a70ff6f12942793c366
Paul Gonzalez <paulgonzalez@Pauls-MacBook-Pro.local>
2021-08-04 09:23:28 -0700

    MVP complete and adding graphs

--- OnlyStonks/app.js
+++ OnlyStonks/app.js
@@ -1,29 +1,65 @@
 
 // API http://api.marketstack.com/v1/tickers?access_key=b9a441095845496dd1fb8777012283f3
 
-
-// const tickerURL = "http://api.marketstack.com/v1/tickers?access_key=b9a441095845496dd1fb8777012283f3"
-
-// const searchBar = document.getElementById('search').querySelector('input')
-// searchBar.addEventListener('keyup', (e) => {
-//   const term = e.target.value.upperCase
-// })
-
+const stonkResults = document.querySelector('.stonk-results')
+// console.log(stonkResults)
 
 async function getTickerData(e) {
+  removeElement(stonkResults)
   try {
     e.preventDefault()
     let { search } = e.target
     let response = await axios.get(`http://api.marketstack.com/v1/tickers/${search.value}/eod?access_key=b9a441095845496dd1fb8777012283f3`)
-    // let tickerData = response.data.data.eod[0]
-    // console.log(tickerData)
+    const tickerData = response.data.data.eod[0]
+    const tickerDataYear = response.data.data.eod
+    console.log(tickerData)
+    console.log(tickerDataYear)
+
+    const stonkDiv = document.createElement('div')
+    stonkResults.appendChild(stonkDiv)
+
     const symbol = response.data.data.eod[0].symbol
+    const symbolElement = document.createElement('h2')
+    symbolElement.textContent = `${symbol}`
+    // console.log(symbolElement)
+    symbolElement.classList.add('symbol-style')
+    stonkDiv.appendChild(symbolElement)
+
     const open = response.data.data.eod[0].open
+    const openElement = document.createElement('p')
+    openElement.textContent = `Open: ${open}`
+    // console.log(openElement)
+    openElement.classList.add('open-style')
+    stonkDiv.appendChild(openElement)
+
     const close = response.data.data.eod[0].close
+    const closeElement = document.createElement('p')
+    closeElement.textContent = `Close: ${close}`
+    // console.log(closeElement)
+    closeElement.classList.add('close-style')
+    stonkDiv.appendChild(closeElement)
+
     const high = response.data.data.eod[0].high
+    const highElement = document.createElement('p')
+    highElement.textContent = `High: ${high}`
+    // console.log(highElement)
+    highElement.classList.add('high-style')
+    stonkDiv.appendChild(highElement)
+
     const low = response.data.data.eod[0].low
+    const lowElement = document.createElement('p')
+    lowElement.textContent = `Low: ${low}`
+    // console.log(lowElement)
+    lowElement.classList.add('low-style')
+    stonkDiv.appendChild(lowElement)
+
     const volume = response.data.data.eod[0].volume
-    console.log(symbol, open, close, high, low, volume)
+    const volumeElement = document.createElement('p')
+    volumeElement.textContent = `Volume: ${volume}`
+    // console.log(volumeElement)
+    volumeElement.classList.add('volume-style')
+    stonkDiv.appendChild(volumeElement)
+
   } catch (error) {
     console.error(error)
   }
@@ -33,5 +69,10 @@ async function getTickerData(e) {
 const form = document.querySelector('form')
 form.addEventListener('submit', getTickerData)
 
+function removeElement(element) {
+  while (element.lastChild) {
+    element.removeChild(element.lastChild)
+  }
+}
 
 
